0340df85f40ca94b742e77bfaede1593758c9206
zhanglingfu123456 <704921095@qq.com>
2019-04-12 13:48:02 +0800

    Update IdMetaFactory.java
    fix bit

--- vesta-service/src/main/java/com/robert/vesta/service/impl/bean/IdMetaFactory.java
+++ vesta-service/src/main/java/com/robert/vesta/service/impl/bean/IdMetaFactory.java
@@ -2,11 +2,11 @@ package com.robert.vesta.service.impl.bean;
 
 public class IdMetaFactory {
 
-    private static IdMeta maxPeak = new IdMeta((byte) 10, (byte) 20, (byte) 30, (byte) 1, (byte) 1, (byte) 1);
+    private static IdMeta maxPeak = new IdMeta((byte) 10, (byte) 20, (byte) 30, (byte) 2, (byte) 1, (byte) 1);
 
-    private static IdMeta minGranularity = new IdMeta((byte) 10, (byte) 10, (byte) 40, (byte) 1, (byte) 1, (byte) 1);
+    private static IdMeta minGranularity = new IdMeta((byte) 10, (byte) 10, (byte) 40, (byte) 2, (byte) 1, (byte) 1);
 
-    private static IdMeta shortId=new IdMeta((byte)10,(byte) 10,(byte) 30,(byte)1,(byte) 1,(byte) 1);
+    private static IdMeta shortId=new IdMeta((byte)10,(byte) 10,(byte) 30,(byte)2,(byte) 1,(byte) 1);
     public static IdMeta getIdMeta(IdType type) {
         if (IdType.SECONDS.equals(type)) {
             return maxPeak;
